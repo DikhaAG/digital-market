49a0fed9d76caf6e80a3b1fe40c2a2e42eb477e8
DikhaAG <dikhaagsfrdy@gmail.com>
2026-07-26 12:25:43 +0700

    feat: nambahin origin domain untuk kebutuhan development local

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+    "192.168.18.106:3000",
+    "192.168.18.106",
+    "localhost:3000",
+  ],
 };
 
 export default nextConfig;
