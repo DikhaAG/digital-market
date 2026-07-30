dca13a0e0c85426719c40195a9a04be45b9c1054
DikhaAG <dikhaagsfrdy@gmail.com>
2026-07-31 00:41:39 +0700

    feat: addding hero section

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,3 +1,10 @@
+import { HeroSection } from "./_components/HeroSection";
+
 export default function UserPage() {
-  return <div>User page</div>;
+  return (
+    <div className="w-full space-y-12">
+      <HeroSection />
+      {/* Seksi konten user page lainnya dapat diletakkan di bawah sini */}
+    </div>
+  );
 }
