806959d4a9c6341e10c1e29a69543d12bd9e9456
DikhaAG <dikhaagsfrdy@gmail.com>
2026-07-31 02:48:49 +0700

    chore

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -4,7 +4,6 @@ export default function UserPage() {
   return (
     <div className="w-full space-y-12">
       <HeroSection />
-      {/* Seksi konten user page lainnya dapat diletakkan di bawah sini */}
     </div>
   );
 }
